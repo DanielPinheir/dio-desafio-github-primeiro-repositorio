ecddbf726ee1154277eeab189f61cae8c30ef7f4
DanielPinheir <dyelzim@gmail.com>
2022-01-04 20:13:39 -0300

    introdução a javascript

---
+++ Javascript_inicio/teste.js
@@ -0,0 +1,22 @@
+//cometários//
+/*var y = 0;
+var x = 1; */
+
+var preco = 2;
+var desconto = 0.2;
+//boa prática declarar constantes em letra maiúscula
+//constante não é dinâmico, sempre será aquele valor
+const PRECO = 2;
+var total = PRECO - desconto;
+
+
+//Funções
+function soma (/*atributos, parâmetros*/a,b) {
+    //console.log(a + b);  usado para visulizar, verificar se está funcionando
+    return a + b;       
+}
+
+soma(3, 5); //chamando a função
+
+
+
